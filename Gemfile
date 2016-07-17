fc5853ad5c5ce51636b693f4ef56735b4779a8a5
James Naadjie <james.naadjie@gmail.com>
2016-07-17 10:00:39 -0400

    add practice area extension

--- Gemfile
+++ Gemfile
@@ -67,3 +67,5 @@ gem 'refinerycms-authentication-devise', '~> 1.0'
 #  gem 'refinerycms-inquiries', ['~> 3.0', '>= 3.0.0']
 #  gem 'refinerycms-search', ['~> 3.0', '>= 3.0.0']
 #  gem 'refinerycms-page-images', ['~> 3.0', '>= 3.0.0']
+
+gem 'refinerycms-practice_areas', path: 'vendor/extensions'
---
+++ db/migrate/20160716182402_create_practice_areas_practice_areas.refinery_practice_areas.rb
@@ -0,0 +1,29 @@
+# This migration comes from refinery_practice_areas (originally 1)
+class CreatePracticeAreasPracticeAreas < ActiveRecord::Migration
+
+  def up
+    create_table :refinery_practice_areas do |t|
+      t.string :name
+      t.text :blurb
+      t.text :description
+      t.integer :position
+
+      t.timestamps
+    end
+
+  end
+
+  def down
+    if defined?(::Refinery::UserPlugin)
+      ::Refinery::UserPlugin.destroy_all({:name => "refinerycms-practice_areas"})
+    end
+
+    if defined?(::Refinery::Page)
+      ::Refinery::Page.delete_all({:link_url => "/practice_areas/practice_areas"})
+    end
+
+    drop_table :refinery_practice_areas
+
+  end
+
+end
