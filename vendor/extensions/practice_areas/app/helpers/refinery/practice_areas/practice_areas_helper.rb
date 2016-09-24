a1043df241cab61663c34e510376197a1ff0fe6c
James Naadjie <james.naadjie@gmail.com>
2016-09-23 23:12:18 -0400

    change indents from tabs to space

--- vendor/extensions/practice_areas/app/helpers/refinery/practice_areas/practice_areas_helper.rb
+++ vendor/extensions/practice_areas/app/helpers/refinery/practice_areas/practice_areas_helper.rb
@@ -1,10 +1,10 @@
 module Refinery
-	module PracticeAreas
-		module PracticeAreasHelper
-			def trim_text(text,path = '#')
-				truncate(text,length: 520,:escape => false) {link_to 'more info', path }
-			end
-		end
-	end
+  module PracticeAreas
+    module PracticeAreasHelper
+      def trim_text(text,path = '#')
+        truncate(text,length: 520,:escape => false) {link_to 'more info', path }
+      end
+    end
+  end
 end
 
