27051a6b57a9b759329600b68214b9af43ab8540
James Naadjie <jnaadjie@gmail.com>
2017-09-09 00:11:48 -0400

    Add press_releases helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,11 @@
 module ApplicationHelper
+  def press_releases(news_items)
+    news_items.select do |item|
+      item.section.name == 'press';
+    end
+  end
   def item_link(item)
-    if item.section.name === 'press'
+    if item.section.name == 'press'
       link_to item.title.truncate(35), refinery.media_news_presses_media_news_press_path(item)
     else
       link_to item.title.truncate(35), item.link
